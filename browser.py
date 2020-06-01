f70220204e7470f3a928f352f375fd4561acd7d2
sunxueliang96 <sundianzhang@icloud.com>
2020-06-01 21:32:48 +0800

    update

--- browser.py
+++ browser.py
@@ -9,13 +9,19 @@ config = read_config('config')
 TBB_dir = config['TBB_dir']
 firefox_dir = config['firefox_dir']
 
+proxy_type = int(config['proxy_type'])
+proxy_http = str(config['proxy_http'])
+proxy_http_port = int(config['proxy_http_port'])
+proxy_ssl = str(config['proxy_ssl'])
+proxy_ssl_port = int(config['proxy_ssl_port'])
+
 def firfox_proxy(webdriver):
     profile = webdriver.FirefoxProfile()
-    profile.set_preference('network.proxy.type', 1)
-    profile.set_preference('network.proxy.http', '::1')
-    profile.set_preference('network.proxy.http_port', 8899)
-    profile.set_preference('network.proxy.ssl', '::1')
-    profile.set_preference('network.proxy.ssl_port', 8899)
+    profile.set_preference('network.proxy.type', proxy_type)
+    profile.set_preference('network.proxy.http', proxy_http)
+    profile.set_preference('network.proxy.http_port', proxy_http_port)
+    profile.set_preference('network.proxy.ssl', proxy_ssl)
+    profile.set_preference('network.proxy.ssl_port', proxy_ssl_port)
     profile.update_preferences()
     return profile
 
